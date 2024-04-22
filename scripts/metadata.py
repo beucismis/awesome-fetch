9f475807913e95c5d38081bee35e0659866cc5d9
beucismis <beucismis@tutamail.com>
2024-04-22 21:14:49 +0300

    Fix metadata.py

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -15,10 +15,12 @@ class Tool:
 README_FILE = sys.argv[1]
 METADATA_FILE = sys.argv[2]
 
-template_pattern = r"- \[(.*?)\]\((.*?)\) - (.*?)\. \`([^\`]+)\`"
+template_pattern = r"-\s\[(.*?)\]\((.*?)\)\s-\s(.*?)\s\`([^\`]+)\`"
 matches = re.findall(template_pattern, open(README_FILE).read())
 tools = [asdict(Tool(*match)) for match in matches]
 metadata_content = {"total": len(tools), "tools": tools}
 
-with open(METADATA_FILE, "w+") as file:
-    json.dump(metadata_content, file, indent=2)
+with open(METADATA_FILE, "w+", encoding="utf-8") as file:
+    json.dump(metadata_content, file, indent=2, ensure_ascii=False)
+
+sys.exit("Done!")
