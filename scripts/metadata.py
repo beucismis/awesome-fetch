c3caff838f757d0c57081f5b6d5327db9bbd6865
beucismis <beucismis@tutamail.com>
2024-04-22 20:47:07 +0300

    Add metadata builder

---
+++ scripts/metadata.py
@@ -0,0 +1,24 @@
+import json
+import re
+import sys
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class Tool:
+    name: str
+    url: str
+    description: str
+    language: str
+
+
+README_FILE = sys.argv[1]
+METADATA_FILE = sys.argv[2]
+
+template_pattern = r"- \[(.*?)\]\((.*?)\) - (.*?)\. \`([^\`]+)\`"
+matches = re.findall(template_pattern, open(README_FILE).read())
+tools = [asdict(Tool(*match)) for match in matches]
+metadata_content = {"total:": len(tools), "tools": tools}
+
+with open(METADATA_FILE, "w+") as file:
+    json.dump(metadata_content, file, indent=2)
